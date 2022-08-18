17255876e5d619a9f4d3a188d3e029b05d01d6fa
yashgaikwad_crest <yash.gaikwad@crestdatasys.com>
2022-08-18 15:08:19 +0530

    CREST-134: Resolved PR comments; removed Duplicated files, added custom exception class

--- Packs/Illumio/Integrations/IllumioCore/IllumioCore_test.py
+++ Packs/Illumio/Integrations/IllumioCore/IllumioCore_test.py
@@ -11,19 +11,19 @@ def mock_client():
     """Mock a client object with required data to mock."""
     from IllumioCore import IllumioClient
 
-    client = IllumioClient(
+    return IllumioClient(
         base_url="https://127.0.0.1:8443",
+        port=8443,
         org_id=1,
         api_user="dummy",
         api_key="dummy-1",
         proxy={}
     )
-    return client
 
 
 def util_load_json(path):
     """Load a JSON file to python dictionary."""
-    with io.open(path, mode='r', encoding='utf-8') as f:
+    with io.open(path, mode="r", encoding="utf-8") as f:
         return json.loads(f.read())
 
 
@@ -32,13 +32,13 @@ def test_test_module(requests_mock, mock_client):
     Test case scenario for successful execution of test_module.
 
     Given:
-       - mocked client
+       - mocked client.
     When:
-       - Calling `test_module` function
+       - Calling `test_module` function.
     Then:
-       - Returns an ok message
+       - Returns an ok message.
     """
     from IllumioCore import test_module
-    requests_mock.get("https://127.0.0.1:8443/api/v2/orgs/1/workloads",
+    requests_mock.get("https://127.0.0.1:8443/api/v2/health",
                       status_code=200, json={})
-    assert test_module(mock_client) == 'ok'
+    assert test_module(mock_client) == "ok"
